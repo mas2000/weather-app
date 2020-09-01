25e28bda794be04762f0fcd3c37528a5e0de9bfe
mas2000 <69626496+mas2000@users.noreply.github.com>
2020-09-01 23:41:22 +0500

    Add files via upload

---
+++ precache-manifest.2d52b2acf167a668f07f5d8d2d66de87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f3baacbc2baff2a25399ecc779c729",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2993b300dde8672fd9a",
+    "url": "/static/css/main.28427520.chunk.css"
+  },
+  {
+    "revision": "df886f85a5ac6c169c18",
+    "url": "/static/js/2.817d298a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.817d298a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2993b300dde8672fd9a",
+    "url": "/static/js/main.1445f5a2.chunk.js"
+  },
+  {
+    "revision": "acdded0c6e86204e8f84",
+    "url": "/static/js/runtime-main.61b1d4d9.js"
+  }
+]);
